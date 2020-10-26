7561ef95a0b61ee281ecd0b00a3f39f2eddd9e53
Pranav102 <pranav9456667652@gmail.com>
2020-10-26 10:18:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+// import { Typewriter } from 'react-typewriting-effect'
+// import 'react-typewriting-effect/dist/index.css'
+import Typewriter from 'typewriter-effect';
 
-function App() {
+
+// import { AnimateOnChange } from 'react-animation'
+ 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+ 
+<Typewriter
+  options={{
+    strings: ['Hello World', 'wait',"OMG","u guys have been hacked <../..>","cheers!!"],
+    autoStart: true,
+    loop: true
+    }}
+/>
+    
+//     <div className="ll">
+//     <Typewriter
+//       string='Hello World'
+//       delay={200}
+//       cursor= "_"
+//       stopBlinkinOnComplete={false}
+//       />
+// <AnimateOnChange
+//   animationIn="custom-animation-in 500ms ease-out forwards"
+//   animationOut="custom-animation-out 500ms ease-out forwards"
+//   durationOut={500}
+// >
+// Perfect
+// </AnimateOnChange>
+//     </div>
+    )
 }
-
-export default App;
+ 
+export default App
